8cec1af34a4f8ba4151cdcd17f08178f4ddf54c7
KitchenNuggets <83659500+Kitchen-Nuggets@users.noreply.github.com>
2022-10-07 13:44:14 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("--------HELLO JENKINS GITHUB V1--------");
+		System.out.println("--------HELLO JENKINS GITHUB V2--------");
 	}
 }
